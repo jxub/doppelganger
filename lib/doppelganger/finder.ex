a59463ab06e5ef54f8ddf6bb3bc781803dc85422
Jakub Janarek <jjanarek@gmail.com>
2018-09-05 23:44:30 +0200

    add finder module

--- lib/doppelganger/finder.ex
+++ lib/doppelganger/finder.ex
@@ -1,1 +1,19 @@
+defmodule Doppelganger.Finder do
+  use Task
 
+  def start_link(arg) do
+    Task.start_link(__MODULE__, :run, [arg])
+  end
+
+  def run(arg) do
+    # ...
+  end
+
+  def run({elem, code}) do
+    
+  end
+
+  def run({:struct, code}) do
+    
+  end
+end
